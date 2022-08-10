09437c500c5aa46f6f30eab1b8410c97025383b3
Art Cuddy <art.cuddy@dmgmedia.ie>
2022-08-10 09:32:06 +0000

    Fix: cleaned up some of the pylint errors

--- cocktailapp/models.py
+++ cocktailapp/models.py
@@ -13,7 +13,7 @@ class Post(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="blog_posts"
     )
-    featured_image = CloudinaryField('image', default='placeholder')
+    featured_image = CloudinaryField("image", default="placeholder")
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
@@ -21,7 +21,7 @@ class Post(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     status = models.IntegerField(choices=STATUS, default=0)
     likes = models.ManyToManyField(
-        User, related_name='blogpost_like', blank=True)
+        User, related_name="blogpost_like", blank=True)
 
     class Meta:
         ordering = ["-created_on"]
