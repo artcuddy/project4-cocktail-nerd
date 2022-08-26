f7309eeda40cb6d327a0bf3b60186d80a0144b00
Art Cuddy <art.cuddy@dmgmedia.ie>
2022-08-26 10:07:38 +0000

    Add: display current posts category tag and link to category page

--- cocktailapp/views.py
+++ cocktailapp/views.py
@@ -66,6 +66,7 @@ class PostDetail(View):
 
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
+        cat_posts = Post.objects.filter(categories=post.category)
         comments = post.comments.filter(approved=True).order_by("-created_on")
         liked = False
         if post.likes.filter(id=self.request.user.id).exists():
@@ -86,6 +87,7 @@ class PostDetail(View):
             "post_detail.html",
             {
                 "post": post,
+                'cat_posts': cat_posts,
                 "comments": comments,
                 "commented": True,
                 "comment_form": comment_form,
