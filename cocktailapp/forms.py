58f94691a0ff089fc24c59a1ddc18f3b9f4a4f67
Art Cuddy <art.cuddy@dmgmedia.ie>
2022-09-08 11:10:45 +0000

    Style: tightend up the padding on the mobile view

--- cocktailapp/forms.py
+++ cocktailapp/forms.py
@@ -14,8 +14,8 @@ class CocktailForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = (
-            'status',
             'title',
+            'status',
             'author',
             'categories',
             'featured_image',
@@ -26,8 +26,8 @@ class CocktailForm(forms.ModelForm):
             )
 
         widgets = {
-            'status': forms.Select(attrs={'class': 'form-control'}),
             'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'status': forms.Select(attrs={'class': 'form-control'}),
             'author': forms.Select(attrs={'class': 'form-control'}),
             'categories': forms.Select(attrs={'class': 'form-control'}),
             'content': SummernoteWidget(),
