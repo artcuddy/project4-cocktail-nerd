31fa0f255d682d4a1a4ac542f0fc793c55e29218
Art Cuddy <art.cuddy@dmgmedia.ie>
2022-09-11 14:34:00 +0000

    Add: total user likes to navbar when logged in

--- cocktailapp/views.py
+++ cocktailapp/views.py
@@ -171,6 +171,7 @@ class PostLike(View):
 
 
 # Liked posts view
+@login_required
 def liked_list(request):
     liked_list = Post.objects.filter(likes=request.user)
     total_liked_list = liked_list.count()
@@ -254,7 +255,7 @@ class SearchResultsView(ListView):
     model = Post
     template_name = 'search_posts.html'
 
-    def get_queryset(self):  # new
+    def get_queryset(self):
         query = self.request.GET.get("searched", default="")
         object_list = Post.objects.filter(
             Q(title__icontains=query) | Q(ingredients__icontains=query)
